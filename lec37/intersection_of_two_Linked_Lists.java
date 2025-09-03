4246675fb2a47a54e31a9a360983e77bc18ed5a7
Rachit Upadhyay <urachit08@gmail.com>
2025-09-03 23:17:22 +0530

    Linked List

--- lec37/intersection_of_two_Linked_Lists.java
+++ lec37/intersection_of_two_Linked_Lists.java
@@ -10,9 +10,26 @@ public class intersection_of_two_Linked_Lists {
        }
     }
     public class Solution {
-        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {   
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode v=headA;
+            ListNode vgf=headB;
+            while(v != vgf){
+                if(v==null){
+                    v=headB;
+                }
+                else{
+                    v=v.next;
+                }
+                if(vgf==null){
+                    vgf=headA;    
+                }
+                else{
+                    vgf=vgf.next;
+                }
+            }                    
+            return v; 
         }
-}    
+    }    
 }
 
 // leetcode 160
